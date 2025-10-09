88e45186648e3044434e96ae1f1d8d20efe15434
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-09 12:34:12 +0530

    Test

---
+++ main.py
@@ -0,0 +1,14 @@
+from calculations import multiply, subtract, sum
+
+
+def main(a: int, b: int):
+    addition = sum(a, b)
+    subtraction = subtract(a, b)
+    multiplication = multiply(a, b)
+    result = multiplication + addition + subtraction
+    return result
+
+
+if __name__ == "__main__":
+    output = main(4, 8)
+    print(f"Result is: {output}")
