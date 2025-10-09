a410a900e0cea3ac442c73c5d372d1d0a5be98a4
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-09 13:02:40 +0530

    test

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from calculations import multiply, subtract, sum
 
 def main(a: int, b: int):
     addition = sum(a, b)
-    subtraction = subtract(a, b)
-    multiplication = multiply(a, b)
+    subtraction = subtract(c, b)
     result = multiplication + addition + subtraction
     return result
 
