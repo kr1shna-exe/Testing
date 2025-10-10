89b064ad4eae91377036a00ea951606b2a43c56c
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-10 11:20:52 +0530

    Fix

--- calculations.py
+++ calculations.py
@@ -1,5 +1,5 @@
 def sum(a: int, b: int):
-    return a + b + d
+    return a + b
 
 
 def multiply(a: int, b: int):
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from calculations import multiply, subtract, sum
 def main(a: int, b: int):
     addition = sum(a, b)
     multiplication = multiply(a, b)
-    result = multiplication + addition+ subtracti
+    result = multiplication + addition + subtracti
     return result
 
 
-if __name _=== "__main__":
+if __name__ == "__main__":
     output = main(4, 8)
     print(f"Result is: {output}")
