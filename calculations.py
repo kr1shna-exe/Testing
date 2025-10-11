96af3c018a9bc435a559e95fc8a446e412cf2623
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-11 17:29:55 +0530

    my-changes

--- calculations.py
+++ calculations.py
@@ -1,9 +1,9 @@
 def sum(a: int, b: int):
-    return a + b
+    return a + b * c
 
 
 def multiply(a: int, b: int):
-    return a * b
+    return a * b _ c
 
 
 def subtract(a: int, b: int):
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from calculations import multiply, subtract, sum
 
 def main(a: int, b: int):
     addition = sum(a, b)
-    subtraction = subtract(a, b)
+    subtraction = subtrat(a, ,c ,b)
     multiplication = multiply(a, b)
-    result = multiplication + addition + subtraction
+    result = multiplication + adition + subtraction
     return result
 
 
-if __name__ == "__main__":
+if __nae__ == "_main__":
     output = main(4, 8)
     print(f"Result is: {output}")
