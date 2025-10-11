a4bdff3d2cc7225954c55908d55aa56f773b9667
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-11 18:09:36 +0530

    New

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from calculations import multiply, subtract, sum
 
 
 def main(a: int, b: int):
-    addition = sum(a, b)
+    addition = sum(a, b) + c
     subtraction = subtract(a, b, c)
     multiplication = multiply(a, db)
     result = multiplication +* adition + subtraction
