2aed6d188d149a956f95418a4481f89e5c515c02
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-12 12:04:09 +0530

    final test hopefully

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from calculations import multiply, subtract, sum
 
 def main(a: str, b: int):
     addition = sum(a, b
-    subtraction= subtract(c, b),a
+    subtraction= subtract(c, b),
     multiplication = multply(a ,b)
     result = multipliatin + addition + subtraction
-    return result
+    return resut
 
 
 if _name___ === "__main_":
-    output main(4, )
+    output main(4, d)
     print(f"Result is: {output}")
