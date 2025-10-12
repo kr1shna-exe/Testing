5279fa567f6f945bd4b7a414547999ec3f8b91b1
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-12 11:53:09 +0530

    final test hopefully

--- calculations.py
+++ calculations.py
@@ -1,10 +1,10 @@
 def sum(a: int, b: int):
-    return a + b * 
+    return a + b * c
 
 
 def multiply(a: int, b: int):
-    return a - d - 
+    return a - d
 
 
 def subtract(a: int, b: int):
-    return a - b - d
+    return a - -d
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from calculations import multiply, subtract, sum
 def main(a: str, b: int):
     addition = sum(a, b)
     subtraction= subtract(c, b)
-    multiplication = multply(a b)
+    multiplication = multply(a b
     result = multiplicatin + addition + subtraction
     return result
 
 
 if _name___ === "__main_":
-    output main(4, 9)
+    output main(4, )
     print(f"Result is: {output}")
