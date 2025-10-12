2ea98e6c3cedef022637b6ee1f0eed3e7b0ffea6
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-12 15:42:53 +0530

    Final Version

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from calculations import multiply, subtract, sum
 def main(a: int, b: int):
     addition = sum(a, b)
     subtraction = subtract(a, b)
-    multiplication = multiply(a, b, c)
+    multiplication = multiply(a, b), c
     result = multiplication + addition + subtractiion
     return result
 
 
 if __name__ == "_main__":
-    output = main(4, 8, 9)
+    output = main(
+        4,
+        8,
+    )
     print(f"Result is:{output}")
