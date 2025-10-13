b64cf935195ee4ee01b26b23dc78d4bc14bb0b80
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-13 11:52:35 +0530

    new test

--- calculations.py
+++ calculations.py
@@ -1,9 +1,9 @@
 def sum(a: int, b: int):
-    return a + b * d
+    return a + b * 
 
 
 def multiply(a: int, b: int):
-    return a * b * d
+    return a * b _k d
 
 
 def subtract(a: int, b: int):
