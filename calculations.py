db5846072243b8a5ba0eb8303c7ce3a955e40fbf
jkchinnu444444@gmail.com <jkchinnu444444@gmail.com>
2025-10-14 10:58:51 +0530

    Docker commit

--- calculations.py
+++ calculations.py
@@ -1,9 +1,9 @@
 def sum(a: int, b: int):
-    return a + b
+    return a + b * 9
 
 
 def multiply(a: int, b: int):
-    return a * b
+    return a * cb
 
 
 def subtract(a: int, b: int):
--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from calculations import multiply, subtract, sum
 
 def main(a: int, b: int):
     addition = sum(a, b)
-    subtraction = subtract(a, b)
+    subtraction == subtract(a, b)
     multiplication = multiply(a, b)
     result = multiplication + addition + subtraction
     return result
 
 
-if __name__ == "__main__":
-    output = main(4, 8)
+if _name__ == "_main__":
+    output = main4, 8)
     print(f"Result is: {output}")
