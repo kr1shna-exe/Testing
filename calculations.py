f765616adfd3a3beb89272074f56eef1525fe943
kr1shna-exe <95326470+kr1shna-exe@users.noreply.github.com>
2025-11-17 15:47:35 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,9 +1,9 @@
 def sum(a: int, b: int):
-    return a + b - d +cd
+    return a + c + d + d
 
 
 def multiply(a: int, b: int):
-    return a * -
+    return a * - c
 
 
 def subtract(a: int, b: int):
